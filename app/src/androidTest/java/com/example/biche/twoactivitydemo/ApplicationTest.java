cddb7da49e030c9d29d85057e15727bfebd86169
Сергей <bichevoi@gmail.com>
2016-03-16 22:25:14 +0600

    Initial commit

---
+++ app/src/androidTest/java/com/example/biche/twoactivitydemo/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.biche.twoactivitydemo;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
